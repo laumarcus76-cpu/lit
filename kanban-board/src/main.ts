1212b93c762ddd6e5fbcbb13ffedbf33f82a76eb
Marcus Lau <lau.marcus76@gmail.com>
2026-02-26 19:58:15 -0800

    ticket 1 done

---
+++ kanban-board/src/main.ts
@@ -0,0 +1,5 @@
+// Component imports will be added here as each ticket is completed
+// TICKET-03: import './components/task-card.js';
+// TICKET-04: import './components/task-column.js';
+// TICKET-05: import './components/task-board.js';
+// TICKET-06: import './components/add-task-modal.js';
